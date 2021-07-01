ed9b65ac6837bda123a58390aa62cdfb61d2b035
zebrah60312 <surapan.ku@gmail.com>
2021-07-01 14:45:17 +0700

    add file composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7b1e177679d04c3fad3fcadc5e995cdd5d161a4c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7b1e177679d04c3fad3fcadc5e995cdd5d161a4c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
